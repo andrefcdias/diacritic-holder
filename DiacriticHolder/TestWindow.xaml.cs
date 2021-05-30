6fd7f1a28f3fc772d70d65df40adbb3bcd56c483
André Dias <39736248+andrefcdias@users.noreply.github.com>
2021-05-30 20:26:01 +0200

    Add test window

---
+++ DiacriticHolder/TestWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace DiacriticHolder
+{
+    public sealed partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
